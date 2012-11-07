d4459a1ade836bdd81fc2135559ddc9589d03f1e
Bruce Crevensten <bruce.crevensten@gmail.com>
2012-11-07 14:54:36 -0900

    #2, #3 enough tests to get valid failures for mocha

--- test/mocha/connection.js
+++ test/mocha/connection.js
@@ -1,6 +1,7 @@
-var SerialPort = require("serialport").SerialPort;
-var MAVLink = require("node-mavlink");
-var Connection = require("gcs-connection");
+var SerialPort = require("serialport").SerialPort
+  , Connection = require("../../lib/Connection.js")
+  , MAVLink = require("../../lib/MAVLink")
+  , sinon = require("sinon");
 
 describe("Connection manager", function() {
 
@@ -9,7 +10,7 @@ describe("Connection manager", function() {
     this.connection = new Connection;
 
     // Use this: http://code.google.com/p/macosxvirtualserialport/
-    // or similar.  Set up a serial port connection, 
+    // or similar.  Set up a serial port connection,   
     this.serial = new SerialPort("/dev/master", {
       baudrate: 9600
     });
@@ -18,7 +19,7 @@ describe("Connection manager", function() {
     });
 
     this.connection.setBuffer(slave);
-    this.connection.setProtocol(new Mavlink);
+    this.connection.setProtocol(new MAVLink);
 
   });
 
@@ -29,11 +30,13 @@ describe("Connection manager", function() {
   // Point being the client can provision the connection, so various
   // buffer/protocol combinations are possible.
   it("has an IO buffer", function() {
-    this.connection.should.have.ownProperty('buffer');
+    this.connection.should.have.a.property('buffer');
+    this.connection.buffer.should.be.a('object');
   });
 
   it("has a protocol en/decoder", function() {
-    this.connection.should.have.ownProperty('protocol');
+    this.connection.should.have.a.property('protocol');
+    this.connection.protocol.should.be.a('object');
   });
 
   it("watches the data event on the buffer and decodes data whenever possible", function() {
@@ -43,8 +46,10 @@ describe("Connection manager", function() {
   });
 
   // Existence test only, later will test for capture of data
-  it("sends data on demand", function() {
+  it("sends data on demand, encoding it", function() {
+    var spy = sinon.spy(this.connection, 'encode');
     this.connection.send('test data');
+    spy.called.should.equal.true;
   });
 
   // The events that this emits are higher-level than the serial port events,
