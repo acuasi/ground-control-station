1629ff6befd9ff96c633a01961bcdfa8fbe4f148
Bruce Crevensten <bruce.crevensten@gmail.com>
2012-12-13 12:51:39 -0900

    adds /status route to show realtime UAV status

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 var SerialPort = require("serialport").SerialPort
   , mavlink = require("./lib/mavlink_ardupilotmega_v1.0.js")
   , fs = require('fs')
-  , masterSerial = new SerialPort('/dev/tty.usbserial-A900XUV3', {
-    baudrate: 57600
-  })
+  , masterSerial = new SerialPort('/dev/tty.usbserial-A900XUV3', { baudrate: 57600 })
   , mavlinkParser = new MAVLink()
   , express = require('express')
   , app = express()
   , http = require('http')
   , nowjs = require("now")
+  , routes = require('./routes')
+  , path = require('path')
 
-  // Little HTML scratchpad
-  , html = require('fs').readFileSync(__dirname+'/debug.html')
-  // We need to take care with syntax when using Express 3.x and Socket.io.
-  // https://github.com/Flotype/now/issues/200
-  , server = http.createServer(app).listen(3000, function() {
-      console.log('Express server listening on port 3000');
-  })
-  , everyone = nowjs.initialize(server);
+  // Little HTML scratchpad, temporary, will remove
+  , html = require('fs').readFileSync(__dirname+'/debug.html');
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('gobbleblork'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// We need to take care with syntax when using Express 3.x and Socket.io.
+// https://github.com/Flotype/now/issues/200
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+var everyone = nowjs.initialize(server);
 
 mavlinkParser.on('message', function(message) {
-  var messageHtml = '<th scope="row">'
-  + message.name+'</th>'
-  + '<td>'+message.header.seq+'</td>';
-  var payload = '';
-  _.each(message.fieldnames, function(e) {
-    payload += e + "=" + message[e] + ", ";
-  });
-  payload.replace(/, $/,'');
-  console.log(message);
-  messageHtml += '<td>'+payload+'</td>';
-  everyone.now.receiveMessage(messageHtml);
+  everyone.now.receiveMessage(message);
 });
 
 masterSerial.on('data', function(data) {
@@ -41,4 +51,8 @@ masterSerial.on('data', function(data) {
 app.get('/debug', function(req, res) {
   res.set('Content-Type', 'text/html');
   res.send(html);
+});
+
+app.get('/status', function(req, res) {
+  res.render('status', { title: 'Status' } );
 });
