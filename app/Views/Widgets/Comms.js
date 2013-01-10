b3ce1906055b4d0f1a848f66554da4207e5afa61
Rayjan Wilson <rayjan.wilson@gmail.com>
2013-01-10 13:56:55 -0900

    comms widget

--- app/Views/Widgets/Comms.js
+++ app/Views/Widgets/Comms.js
@@ -5,9 +5,20 @@ define(['backbone', 'Templates'], function(Backbone, template) {
     el: '#commsWidget',
     className: 'widget',
     
+    initialize: function() {
+      _.bindAll(this);
+      this.model.on('change:drop_rate_comm', this.render);
+      this.model.on('change:errors_comm', this.render);
+    },
+
     render: function() {
 
-      this.$el.html(template['app/Templates/commsWidget.jade']());
+        this.$el.html(template['app/Templates/commsWidget.jade'](
+            {
+                drop_rate_comm: this.model.get('drop_rate_comm'),
+                errors_comm: this.model.get('errors_comm')
+            }
+        ));
     
     }
     
