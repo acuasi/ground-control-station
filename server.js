7afc9ac32c1ee59dfa35feb541b6c36ea57d0fc0
Rayjan Wilson <rayjan.wilson@gmail.com>
2013-01-14 11:10:41 -0900

    adding more auto-connect stuff. figuring out the javascript callback

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var SerialPort = require("serialport").SerialPort,
   nconf = require("nconf"),
   requirejs = require("requirejs"),
   winston = require("winston"),
+  util   = require('util'),
+  colors = require('colors'),
   child = require("child_process");
 
 requirejs.config({
@@ -32,11 +34,70 @@ var mavlinkParser = new MAVLink(logger);
 // Fetch configuration information.
 nconf.argv().env().file({ file: 'config.json' });
 
+/*
+ * The main Arduino constructor
+ * Connect to the serial port and bind
+ */
+var Board = function (options) {
+  this.log('info', 'initializing');
+  this.debug = options && options.debug || false;
+  this.baudrate = options && options.baudrate || 115200;
+  this.writeBuffer = [];
+
+  var self = this;
+  this.detect(function (err, serial) {
+    if (err) {
+      if(self.listeners('error').length)
+        self.emit('error', err);
+      else
+        throw new Error(err);
+    }else{
+      self.serial = serial;
+      self.emit('connected');
+
+      self.log('info', 'binding serial events');
+      self.serial.on('data', function(data){
+        self.log('receive', data.toString().red);
+        self.emit('data', data);
+      });
+
+      setTimeout(function(){
+        self.log('info', 'board ready');
+        self.sendClearingBytes();
+
+        if (self.debug) {
+          self.log('info', 'sending debug mode toggle on to board');
+          self.write('99' + self.normalizePin(0) + self.normalizeVal(1));
+          process.on('SIGINT', function(){
+            self.log('info', 'sending debug mode toggle off to board');
+            self.write('99' + self.normalizePin(0) + self.normalizeVal(0));
+            delete self.serial;
+            setTimeout(function(){
+              process.exit();
+            }, 100);
+          });
+        }
+
+        if (self.writeBuffer.length > 0) {
+          self.processWriteBuffer();
+        }
+
+        self.emit('ready');      
+      }, 500);
+    }
+  });
+}
+
+/*
+ * EventEmitter, I choose you!
+ */
+util.inherits(UsbConnection, events.EventEmitter);
+
 // Open the serial connection -- TODO, make this resiliant/trying until it finds it / GUI driven, etc.
 // trying to adapt from this code:
 //    https://github.com/ecto/duino/blob/master/lib/board.js
 
-var detect = function(baudrate){
+UsbConnection.prototype.detect = function(callback){
   child.exec('ls /dev | grep usb', function(err, stdout, stderr){
     console.log('Looking for a usbserial device');
       var usb = stdout.slice(0, -1).split('\n'),
@@ -58,7 +119,7 @@ var detect = function(baudrate){
           if (!err) {
             found = temp;
             console.log('Found board at ' + temp.port);
-            return found;
+            break;
           } else {
             //err = new Error('Could not find Arduino');
             console.log('Could not find a usbserial device');
@@ -66,9 +127,19 @@ var detect = function(baudrate){
         }
       }
 
-      //callback(err, found);
+      callback(err, found);
     });
 };
+
+/*
+ * Logger utility function
+ */
+UsbConnection.prototype.log = function (/*level, message*/) {
+  var args = [].slice.call(arguments);
+  if (this.debug) {
+    console.log(String(+new Date()).grey + ' duino '.blue + args.shift().magenta + ' ' + args.join(', '));
+  }
+}
 
 masterSerial = detect(nconf.get('serial:baudrate'));
 /*new SerialPort(
